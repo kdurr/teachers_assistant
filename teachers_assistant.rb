447cb5b9814eb2031de929c35f490618296a49ce
kdurr <kristina.l.durr@gmail.com>
2013-09-01 14:51:41 -0400

    initial commit

---
+++ teachers_assistant.rb
@@ -0,0 +1,14 @@
+require 'csv'
+
+class GradeReader
+  def self.read_file(file_name)
+    mycsv = CSV.read(file_name, headers: true)
+    return_data = {}
+    mycsv["Students"].each_with_index do |student, index|
+      grades_array = mycsv["Grades"][index].split(" ").map { |str| str.to_i }
+      return_data[student] = grades_array
+    end
+    return_data
+  end
+
+end
