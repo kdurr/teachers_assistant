339d652ceb9df5f1c7508b79749341f008bead20
kdurr <kristina.l.durr@gmail.com>
2013-09-01 15:00:01 -0400

    user story 1 done

--- teachers_assistant.rb
+++ teachers_assistant.rb
@@ -4,7 +4,7 @@ class GradeReader
   def self.read_file(file_name)
     mycsv = CSV.read(file_name, headers: true)
     return_data = {}
-    mycsv["Students"].each_with_index do |student, index|
+    mycsv["Student"].each_with_index do |student, index|
       grades_array = mycsv["Grades"][index].split(" ").map { |str| str.to_i }
       return_data[student] = grades_array
     end
@@ -12,4 +12,14 @@ class GradeReader
   end
 end
 
-puts GradeReader.read_file("grades.csv")
+student_grades = GradeReader.read_file("grades.csv")
+
+puts "Printing Student grades: "
+student_grades.each do |student, grades|
+  print "#{student}: "
+  grades.each { |grade| print "#{grade} " }
+  puts
+end
+
+puts
+puts
